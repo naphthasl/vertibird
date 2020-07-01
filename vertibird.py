d0d01c1eb370ac67cef093424a17e386be1b1b56
Naphtha Nepanthez <naphtha@lotte.link>
2020-07-01 17:49:28 +0100

    Added requirements to setup.py

--- vertibird.py
+++ vertibird.py
@@ -22,7 +22,7 @@ from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 
 __author__ = 'Naphtha Nepanthez'
-__version__ = 'RBMK-1000'
+__version__ = '0.0.1'
 __license__ = 'MIT' # SEE LICENSE FILE
 
 GLOBAL_LOOPBACK = '127.0.0.1'
