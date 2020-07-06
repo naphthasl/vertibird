2add32d5917518ab7d1d8aa6d7018374387fb87e
Naphtha Nepanthez <naphtha@lotte.link>
2020-07-06 01:51:33 +0100

    Update version

--- vertibird.py
+++ vertibird.py
@@ -38,7 +38,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.exc import ObjectDereferencedError
 
 __author__ = 'Naphtha Nepanthez'
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 __license__ = 'MIT' # SEE LICENSE FILE
 __all__ = [
     'Vertibird',
